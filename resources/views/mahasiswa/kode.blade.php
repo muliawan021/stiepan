e63025411e11d49382b50f8d1efd0aae3f7bcb97
Iqbal Muliawan <muliawan021@gmail.com>
2017-12-04 18:10:20 +0800

    view form registrasi

--- resources/views/mahasiswa/kode.blade.php
+++ resources/views/mahasiswa/kode.blade.php
@@ -45,15 +45,7 @@
 <script src="{{url('bower_components/jquery/dist/jquery.min.js')}}"></script>
 <script src="{{url('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <script src="{{url('plugins/iCheck/icheck.min.js')}}"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
+
 
 
 
