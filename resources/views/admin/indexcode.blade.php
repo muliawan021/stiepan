0a9639777b2c6eb1a9ef71c1e58dcc72e3fdf36f
Iqbal Muliawan <muliawan021@gmail.com>
2017-12-04 23:16:46 +0800

    view index code->table

--- resources/views/admin/indexcode.blade.php
+++ resources/views/admin/indexcode.blade.php
@@ -4,21 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Index</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="{{url('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{url('bower_components/font-awesome/css/font-awesome.min.css')}}">
+  <link rel="stylesheet" href="{{url('bower_components/Ionicons/css/ionicons.min.css')}}">
+  <link rel="stylesheet" href="{{url('dist/css/AdminLTE.min.css')}}">
+  <link rel="stylesheet" href="{{url('plugins/iCheck/square/blue.css')}}">
+  <link rel="stylesheet" href="{{url('dist/css/skins/_all-skins.min.css')}}">
+  <link rel="stylesheet" href="{{url('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body>
-    <table border="1" >
-        <tr>
-            <th>Nama</th>
-            <th>Kode User</th>
-        </tr>
-        @foreach($kode as $arr)
-        <tr>
-            <td>{{$arr->nama}}</td>
-            <td>{{$arr->kode_user}}</td>
-        </tr>
 
-        @endforeach
-    </table>
+<body>
+<script src="{{url('bower_components/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{url('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{url('plugins/iCheck/icheck.min.js')}}"></script>
+<script src="{{url('bower_components/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{url('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{url('plugins/iCheck/icheck.min.js')}}"></script>
+<script src="{{url('bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{url('bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+<script src="{{url('bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{url('bower_components/fastclick/lib/fastclick.js')}}"></script>
+<script src="{{url('dist/js/adminlte.min.js')}}"></script>
+<script src="{{url('dist/js/demo.js')}}"></script>
+<script>
+  $(function () {
+    $('#example1').DataTable()
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    })
+  })
+</script>
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Hover Data Table</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"></div><div class="col-sm-6"></div></div><div class="row"><div class="col-sm-12"><table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                <thead>
+                <tr role="row">
+                <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Rendering engine: activate to sort column descending" aria-sort="ascending">Nama Mahasiswa</th>
+                <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Rendering engine: activate to sort column descending" aria-sort="ascending">Kode User</th>
+                
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($kode as $arr)
+                <tr role="row" class="odd">
+                  <td class="sorting_1">{{$arr->nama}}</td>
+                  <td>{{$arr->kode_user}}</td>
+                </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+                <tr>
+                <th rowspan="1" colspan="1">Nama Mahasiswa</th>
+                <th rowspan="1" colspan="1">Kode User</th>
+                </tfoot>
+              </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0">6</a></li><li class="paginate_button next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0">Next</a></li></ul></div></div></div></div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    
+        
 </body>
 </html>
